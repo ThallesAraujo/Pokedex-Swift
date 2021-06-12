bf1590bdb99176e87e428c6c3bae5f06c92afc60
Thalles Araújo <thalleshenrique.na@gmail.com>
2021-06-12 12:47:40 -0300

    Initial Commit

---
+++ Pokedex-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokedex-Swift
+//
+//  Created by Thalles Araújo on 12/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
